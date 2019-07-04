76b1160c978a6aa516613fb00f700a2ee21362fd
Dito YP <killcoder212@gmail.com>
2019-07-04 16:45:23 +0700

    init

---
+++ src/components/UserList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="user-list">
+        <h4>Members</h4>
+        <hr>
+        <b-list-group>
+            <b-list-group-item v-for="user in users" :key="user.username">
+                <span v-if="user.presence === 'online'" class="text-success">
+                    ●
+                </span>
+                <span v-if="user.presence === 'offline'" class="text-danger">
+                    ●
+                </span>
+                {{ user.username }}
+            </b-list-group-item>
+        </b-list-group>
+    </div>
+</template>
+
+<script>
+    import { mapState } from 'vuex'
+
+    export default {
+        name: 'user-list',
+        computed: {
+            ...mapState([
+                'loading',
+                'users'
+            ])
+        },
+        methods: {
+            statusColor(status) {
+                return status === 'online' ? 'success' : 'warning'
+            }
+        }
+    }
+</script>
